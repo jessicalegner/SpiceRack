4ba5a3403ef8a73bdc21133d2dc94b4403ca38d8
Jessica Legner <jessicalegner@gmail.com>
2014-04-06 21:31:40 -0400

    Developing the spice detail view. Making call to get one spice by id.

--- www/js/services/spice_service.js
+++ www/js/services/spice_service.js
@@ -3,13 +3,22 @@ app.factory("SpiceService", function($http, $q) {
 
   var getSpicesByUser = function(userId) {
     var deferred = $q.defer();
-    var promise = $http.get('http://localhost:8000/api/v1/spices/user/' + userId).success(function (response) {
+    var promise = $http.get('http://localhost:8000/api/v1/Spices/user/' + userId).success(function (response) {
+        deferred.resolve(response);
+    });
+    return deferred.promise;
+  };
+
+  var getSpiceById = function(id) {
+    var deferred = $q.defer();
+    var promise = $http.get('http://localhost:8000/api/v1/Spices/' + id).success(function (response) {
         deferred.resolve(response);
     });
     return deferred.promise;
   };
 
   return {
-    getSpicesByUser: getSpicesByUser
+    getSpicesByUser: getSpicesByUser,
+    getSpiceById: getSpiceById
   };
 });
